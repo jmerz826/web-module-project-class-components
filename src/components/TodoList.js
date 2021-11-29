2c122f92ea5e0cd80086766b79db80065d7ee5bd
John Merz <jmerz826@gmail.com>
2021-11-29 17:12:26 -0500

    still building functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoList = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>test item</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
